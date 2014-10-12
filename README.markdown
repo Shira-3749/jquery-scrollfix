e1b1dda636ab421e151ca5c5704e1de4b407e505
ShiraNai7 <shiranai7@hotmail.com>
2014-10-12 04:41:41 +0200

    Major refactoring, updated readme, demo

--- README.markdown
+++ README.markdown
@@ -1,130 +1,78 @@
-# jQuery ScrollFix
+# ScrollFix
 
-jQuery plugin to fix an element based on current scrolling position of the page when simple `position: fixed` is not enough.
+jQuery plugin for making an element fixed when it goes out of view.
+
+
+## Demo
 
 See `demo.html` for an example.
 
-## Functionality
 
-- adding a CSS class when the element is supposed to be fixed (its top coordinate is less than the current scroll position)
-- positioning the fixed element correctly even if scrolling horizontally
-- resizing the fixed element if needed
+## Features
+
+- detecting when an element goes out of view and making it fixed
+- replacing the element with an invisible clone that will temporarily occupy space the original spot
+- updating size and position of the fixed element
+
 
 ## Browser support
 
 Tested in Mozilla Firefox, Google Chrome, Safari, Opera and MSIE 7+
 
-## Known limitations
-
-- custom "scrollers" (e.g. a `div` with `overflow: scroll`) are not supported
-
-
-----------
-
 
 ## Usage
 
-The plugin provides single jQuery method you can use.
+The plugin provides a single jQuery method you can use:
 
-### `$(container).scrollFix(element, options);`
 
-- **container** - selector of or element that contains the fixed element
-- **element** - selector of element inside the container that will be fixed
-- **options** - object with various settings (optional)
+### $(element).scrollFix([options])
 
-This method can be called as soon as the given elements exist but usage of `$(document).ready()` is recommended.
+- **element** - element that will be fixed
+- **options** - object with various settings (see list below)
 
-## Required HTML
+**Returns:** an instance of `Shira.ScrollFix.Watcher` or `false` if no element was given / matched.
 
-Example:
-
-    <div id="fixed-container">
-        <div id="fixed-element">
-            Lorem ipsum
-        </div>
-    </div>
-
-## Required CSS
-
-    /* Make the element fixed when it has the scroll-fix class */
-    elementSelector.scroll-fix {
-        position: fixed;
-        top: 0;
-    }
-
-## Return value
-
-The `$().scrollFix()` call returns an object with following methods:
-
-- **update()** - update element's state and dimensions
-- **updateState()** - update element's state
-- **updateDimensions()** - update element's dimensions
 
 ## Options
 
 <table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Default</th>
-      <th>Description</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <tr>
-      <th>scroller</th>
-      <td>window</td>
-      <td>Element to watch for scrolling events, only <code>window</code> is supported now.</td>
-    </tr>
-
-    <tr>
-      <th>elementSubstitute</th>
-      <td>true</td>
-      <td>Create invisible clone of the element to fill the space when the element is fixed.</td>
-    </tr>
-
-    <tr>
-      <th>outerContainer</th>
-      <td>null</td>
-      <td>Element used to determine correct position of the fixed element (<code>body</code> can be used too).</td>
-    </tr>
-
-    <tr>
-      <th>elementFixClass</th>
-      <td>scroll-fix</td>
-      <td>Class added to the element when it is supposed to be fixed.</td>
-    </tr>
-
-    <tr>
-      <th>containerFixClass</th>
-      <td>scroll-fix</td>
-      <td>Class added to the element's container when it is fixed.</td>
-    </tr>
-
-    <tr>
-      <th>fixBoundaryOffset</th>
-      <td>0</td>
-      <td>Offset aplied when detecting whether to fix the element.</td>
-    </tr>
-
-    <tr>
-      <th>unfixBoundaryOffset</th>
-      <td>0</td>
-      <td>Offset aplied when detecting whether to unfix the element.</td>
-    </tr>
-
-    <tr>
-      <th>updateWidth</th>
-      <td>true</td>
-      <td>Update element's width dynamically (requires <strong>elementSubstitute</strong> to be enabled).</td>
-    </tr>
-
-    <tr>
-      <th>onResize</th>
-      <td>null</td>
-      <td>Custom function to call when the fixed element is resized.</td>
-    </tr>
-  </tbody>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Default</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>fixClass</th>
+            <td>"scroll-fix"</td>
+            <td>Class added to the element when it is fixed.</td>
+        </tr>
+        <tr>
+            <th>syncSize</th>
+            <td>true</td>
+            <td>Update element's size when it is fixed.</td>
+        </tr>
+        <tr>
+            <th>syncPosition</th>
+            <td>0</td>
+            <td>Update element's position when it is fixed.</td>
+        </tr>
+        <tr>
+            <th>fixOffset</th>
+            <td>0</td>
+            <td>Offset aplied when detecting whether to fix the element.</td>
+        </tr>
+        <tr>
+            <th>unfixOffset</th>
+            <td>0</td>
+            <td>Offset aplied when detecting whether to unfix the element.</td>
+        </tr>
+        <tr>
+            <th>onUpdateFixed</th>
+            <td>null</td>
+            <td>Custom function to call when the fixed element is updated. The function is passed an instance of <code>Shira.ScrollFix.Watcher</code>.</td>
+        </tr>
+    </tbody>
 </table>
-
---
+++ src/scrollfix.js
@@ -0,0 +1,210 @@
+"use_strict";
+
+var Shira;
+(function (Shira, $) {
+    (function (ScrollFix) {
+        /**
+         * @constructor
+         *
+         * @param {HTMLElement} element DOM element that is going to be fixed
+         * @param {Object}      options option map
+         */
+        ScrollFix.Watcher = function (element, options) {
+            this.element = element;
+            this.options = $.extend({}, ScrollFix.Watcher.defaults, options);
+        };
+
+        ScrollFix.Watcher.defaults = {
+            syncSize: true,
+            syncPosition: true,
+            fixClass: 'scroll-fix',
+            fixTop: 0,
+            fixOffset: 0,
+            unfixOffset: 0,
+            onUpdateFixed: null
+        };
+
+        ScrollFix.Watcher.prototype = {
+            element: null,
+            substitute: null,
+            options: null,
+            fixed: false,
+            attached: false,
+
+            /**
+             * Get absolute X position of the given element
+             *
+             * @param {HTMLElement} elem
+             * @returns {Number}
+             */
+            getElementX: function (elem) {
+                var x = 0;
+                do x += elem.offsetLeft;
+                while (elem = elem.offsetParent);
+
+                return x;
+            },
+
+            /**
+             * Get absolute Y position of the given element
+             *
+             * @param {HTMLElement} elem
+             * @returns {Number}
+             */
+            getElementY: function (elem) {
+                var y = 0;
+                do y += elem.offsetTop;
+                while (elem = elem.offsetParent);
+
+                return y;
+            },
+
+            /**
+             * Fix the element
+             */
+            fix: function () {
+                // create the substitute
+                this.substitute = $(this.element.cloneNode(false))
+                    .css('visibility', 'hidden')
+                    .height($(this.element).height())
+                    .insertAfter(this.element)[0]
+                ;
+
+                // add classes and styles
+                $(this.element)
+                    .css('position', 'fixed')
+                    .css('top', this.options.fixTop + 'px')
+                    .addClass(this.options.fixClass)
+                ;
+            },
+
+            /**
+             * Update the fixed element
+             */
+            updateFixed: function () {
+                // size
+                if (this.options.syncSize) {
+                    $(this.element)
+                        .width($(this.substitute).width())
+                    ;
+                }
+
+                // position
+                if (this.options.syncPosition) {
+                    var currentScrollLeft = $(window).scrollLeft();
+                    var substituteLeftOffset = this.getElementX(this.substitute);
+
+                    $(this.element).css('left', (substituteLeftOffset - currentScrollLeft) + 'px');
+                }
+
+                // callback
+                if (null !== this.options.onUpdateFixed) {
+                    this.options.onUpdateFixed(this);
+                }
+            },
+
+            /**
+             * Unfix the element
+             */
+            unfix: function () {
+                // remove the substitute
+                $(this.substitute).remove();
+                this.substitute = null;
+                
+                // remove classes and styles
+                $(this.element)
+                    .css({
+                        position: '',
+                        left: '',
+                        top: '',
+                        width: '',
+                        height: ''
+                    })
+                    .removeClass(this.options.fixClass)
+                ;
+            },
+
+            /**
+             * Attach the watcher
+             */
+            attach: function () {
+                if (this.attached) {
+                    throw new Error('Already attached');
+                }
+
+                var that = this;
+
+                this.updateEventHandler = function () {
+                    that.pulse();
+                };
+
+                $(window)
+                    .scroll(this.updateEventHandler)
+                    .resize(this.updateEventHandler)
+                ;
+
+                this.attached = true;
+                this.pulse();
+            },
+
+            /**
+             * Detach the watcher
+             */
+            detach: function () {
+                if (!this.attached) {
+                    throw new Error('Not attached');
+                }
+
+                $(window)
+                    .unbind('scroll', this.updateEventHandler)
+                    .unbind('resize', this.updateEventHandler)
+                ;
+
+                this.attached = false;
+            },
+
+            /**
+             * Pulse the watcher
+             */
+            pulse: function () {
+                var currentScroll = $(window).scrollTop();
+
+                if (this.fixed) {
+                    if (currentScroll <= this.getElementY(this.substitute) + this.options.unfixOffset) {
+                        this.unfix();
+                        this.fixed = false;
+                    } else {
+                        this.updateFixed();
+                    }
+                } else {
+                    if (currentScroll >= this.getElementY(this.element) + this.options.fixOffset) {
+                        this.fix();
+                        this.fixed = true;
+                        this.updateFixed();
+                    }
+                }
+            }
+        };
+
+        // jQuery methods
+
+        /**
+         * Attach a watcher to the matched element
+         *
+         * @param {Object} options watcher option map
+         * @returns {ScrollFix.Watcher|Boolean} false if no element was matched
+         */
+        $.fn.scrollFix = function (options) {
+            var element = this[0];
+
+            if (element) {
+                var watcher = new ScrollFix.Watcher(element, options);
+                watcher.attach();
+
+                return watcher;
+            }
+
+            return false;
+        };
+    })(Shira.ScrollFix || (Shira.ScrollFix = {}));
+})(Shira || (Shira = {}), jQuery);
